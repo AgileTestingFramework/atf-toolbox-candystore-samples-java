4189c2dbbb0b9ab4bc5dde92f9f8832bcd333f2a
kmcadams <kyleamcadams@gmail.com>
2017-05-23 20:03:01 -0700

    Created practiceautomation LoadableComponent and PageObject classes

---
+++ candystore-automation-services/src/main/java/com/agiletestingframework/automationpractice/website/AutomationPracticePageFactory.java
@@ -0,0 +1,45 @@
+package com.agiletestingframework.automationpractice.website;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.agiletestingframework.candystore.website.pageobjects.ContactUsPage;
+import com.agiletestingframework.automationpractice.website.pageobjects.IndexPage;
+import com.agiletestingframework.toolbox.ATFHandler;
+
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
+
+@SuppressWarnings("rawtypes")
+public class AutomationPracticePageFactory {
+	private static final Map<String, LoadableComponent> instances = new HashMap<String, LoadableComponent>();
+
+    /** Create all page object constants here for easy access **/
+	public static final String INDEX = "index";
+	public static final String CONTACTUS = "contactUs";
+	
+	private AutomationPracticePageFactory(){}
+	
+	private static synchronized LoadableComponent getInstance(String pageObjectType){
+		
+        // Our "per key" singleton
+        LoadableComponent instance = instances.get(pageObjectType);
+        
+        if (instance == null) {
+            // Lazily create instance
+			if (pageObjectType == INDEX) {
+				instance = new IndexPage();
+			} else if (pageObjectType == CONTACTUS) {
+				instance = new ContactUsPage();
+			}
+
+			// Add it to map
+            instances.put(pageObjectType, instance);
+        }
+        
+		// Initialize the page object elements
+        PageFactory.initElements(ATFHandler.getInstance().getWebAutomation().getWebDriver(), instance);
+
+        return instance;
+	}
+}
