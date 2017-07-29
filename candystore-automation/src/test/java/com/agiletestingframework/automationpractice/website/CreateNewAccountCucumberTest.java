c883191bb5bcb3550e46cad0f03e9af8964b5491
kmcadams <kyleamcadams@gmail.com>
2017-07-29 16:01:08 -0700

    Created Cucumber test for Sign In page, updated step definitions

--- candystore-automation/src/test/java/com/agiletestingframework/automationpractice/website/CreateNewAccountCucumberTest.java
+++ candystore-automation/src/test/java/com/agiletestingframework/automationpractice/website/CreateNewAccountCucumberTest.java
@@ -5,25 +5,26 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.agiletestingframework.automationpractice.website.pageobjects.ContactUsPage;
 import com.agiletestingframework.automationpractice.website.pageobjects.SignInPage;
+import com.agiletestingframework.toolbox.ATFHandler;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
 
 @CucumberOptions(features = "src/test/resources/com/agiletestingframework/automationpractice/features/create-new-account.feature", 
-tags = "@create-account",
-glue= "src/test/java/com/agiletestingframework/automationpractice/stepDefinitions/CreateAccountStepDefs.java")
-public class CreateNewAccountCucumberTest{
+tags = {"@create-account"},
+glue= {"com.agiletestingframework.automationpractice.stepDefinitions"})
+public class CreateNewAccountCucumberTest {
     private TestNGCucumberRunner testNGCucumberRunner;
     private SignInPage signInPage;
     
     @BeforeClass(alwaysRun = true)
     public void setUpClass() throws Exception {
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-		signInPage = (SignInPage)AutomationPracticePageFactory.getInstance(AutomationPracticePageFactory.SIGNIN);
-		signInPage.load();
+//		signInPage = (SignInPage)AutomationPracticePageFactory.getInstance(AutomationPracticePageFactory.SIGNIN);
+//		signInPage.load();
     }
     
     @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
@@ -35,10 +36,12 @@ public class CreateNewAccountCucumberTest{
     public Object[][] features() {
         return testNGCucumberRunner.provideFeatures();
     }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDownClass() throws Exception {
-        testNGCucumberRunner.finish();
-    }
+    
+	@AfterClass(alwaysRun = true)
+	public void ClassTeardown() {
+		ATFHandler.getInstance().teardown();
+		testNGCucumberRunner.finish();
+	}
+    
 
 }
