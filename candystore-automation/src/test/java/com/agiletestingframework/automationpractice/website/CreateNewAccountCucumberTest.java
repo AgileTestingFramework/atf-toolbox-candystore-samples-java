f014eefc923a58a80fb5957313854500bdd05697
kmcadams <kyleamcadams@gmail.com>
2017-07-29 10:47:23 -0700

    created class and initial methods

---
+++ candystore-automation/src/test/java/com/agiletestingframework/automationpractice/website/CreateNewAccountCucumberTest.java
@@ -0,0 +1,44 @@
+package com.agiletestingframework.automationpractice.website;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.agiletestingframework.automationpractice.website.pageobjects.ContactUsPage;
+import com.agiletestingframework.automationpractice.website.pageobjects.SignInPage;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+
+@CucumberOptions(features = "src/test/resources/com/agiletestingframework/automationpractice/features/create-new-account.feature", 
+tags = "@create-account",
+glue= "src/test/java/com/agiletestingframework/automationpractice/stepDefinitions/CreateAccountStepDefs.java")
+public class CreateNewAccountCucumberTest{
+    private TestNGCucumberRunner testNGCucumberRunner;
+    private SignInPage signInPage;
+    
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+		signInPage = (SignInPage)AutomationPracticePageFactory.getInstance(AutomationPracticePageFactory.SIGNIN);
+		signInPage.load();
+    }
+    
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
+
+}
