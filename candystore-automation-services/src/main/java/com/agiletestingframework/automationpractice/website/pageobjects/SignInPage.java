b9b6df5f05f57e8b5f566c92549f13aef8974daf
kmcadams <kyleamcadams@gmail.com>
2017-07-29 15:57:41 -0700

    Added webelements to the page class

--- candystore-automation-services/src/main/java/com/agiletestingframework/automationpractice/website/pageobjects/SignInPage.java
+++ candystore-automation-services/src/main/java/com/agiletestingframework/automationpractice/website/pageobjects/SignInPage.java
@@ -1,10 +1,19 @@
 package com.agiletestingframework.automationpractice.website.pageobjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.LoadableComponent;
 
 import com.agiletestingframework.automationpractice.website.AutomationPracticePageFactory;
 
 public class SignInPage extends LoadableComponent<SignInPage>{
+	
+	@FindBy(name="SubmitCreate")
+	private WebElement createAnAccountBtn;
+	
+	@FindBy(id="email_create")
+	private WebElement emailField;
+	
 	
 	@Override
 	public void load(){
@@ -16,6 +25,24 @@ public class SignInPage extends LoadableComponent<SignInPage>{
 	@Override
 	protected void isLoaded() throws Error {
 		
+	}
+	
+	public boolean createAnAccountBtnIsDisplayed()
+	{
+		boolean success = false;
+		isLoaded();
+		success = createAnAccountBtn.isDisplayed();
+		return success;
+	}
+	
+	public void clickCreateAnAccount()
+	{
+		createAnAccountBtn.click();
+	}
+	
+	public void enterEmailAddress(String email){
+		emailField.click();
+		emailField.sendKeys(email);
 	}
 
 }
